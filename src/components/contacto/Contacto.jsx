9416bec84bd0b0bf1dcc54aba88d195748672e45
EliOlmos <eliolmos81@gmail.com>
2023-05-17 20:40:54 -0300

    contacto y Sobre nosotros agrego contenido

--- src/components/contacto/Contacto.jsx
+++ src/components/contacto/Contacto.jsx
@@ -3,7 +3,59 @@ import React from 'react'
 const Contacto = () => {
   return (
     <div>
-      <h2>En breve más información</h2>
+      <div className="container">
+    <div className="row">
+        <div className="col-md-12">
+            <div className="well well-sm">
+                <form className="form-horizontal" method="post">
+                    <fieldset>
+                        <legend className="text-center header">Contactanos</legend>
+
+                        <div className="form-group">
+                            <span className="col-md-1 col-md-offset-2 text-center"><i className="fa fa-user bigicon"></i></span>
+                            <div className="col-md-8">
+                                <input id="fname" name="name" type="text" placeholder="Nombre" className="form-control"/>
+                            </div>
+                        </div>
+                        <div className="form-group">
+                            <span className="col-md-1 col-md-offset-2 text-center"><i className="fa fa-user bigicon"></i></span>
+                            <div className="col-md-8">
+                                <input id="lname" name="name" type="text" placeholder="Apellido" className="form-control"/>
+                            </div>
+                        </div>
+
+                        <div className="form-group">
+                            <span className="col-md-1 col-md-offset-2 text-center"><i className="fa fa-envelope-o bigicon"></i></span>
+                            <div className="col-md-8">
+                                <input id="email" name="email" type="text" placeholder="Email" className="form-control"/>
+                            </div>
+                        </div>
+
+                        <div className="form-group">
+                            <span className="col-md-1 col-md-offset-2 text-center"><i className="fa fa-phone-square bigicon"></i></span>
+                            <div className="col-md-8">
+                                <input id="phone" name="phone" type="text" placeholder="Telefono" className="form-control"/>
+                            </div>
+                        </div>
+
+                        <div className="form-group">
+                            <span className="col-md-1 col-md-offset-2 text-center"><i className="fa fa-pencil-square-o bigicon"></i></span>
+                            <div className="col-md-8">
+                                <textarea className="form-control" id="message" name="message" placeholder="Dejanos tu consulta y a la brevedad nos comunicaremos. Gracias" rows="7"></textarea>
+                            </div>
+                        </div>
+
+                        <div className="form-group">
+                            <div className="col-md-12 text-center">
+                                <button type="submit" className="btn btn-primary btn-lg">Enviar</button>
+                            </div>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
     </div>
   )
 }
--- src/components/sobreNosotros/SobreNosotros.jsx
+++ src/components/sobreNosotros/SobreNosotros.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import "../sobreNosotros/SobreNosotros.css"
+import React from "react";
+import "../sobreNosotros/SobreNosotros.css";
 
 const SobreNosotros = () => {
   return (
-    <div className='fondo'>
-      <h2>En construcción</h2>
+    <div classNameName="fondo">
+      <div className="card">
+        <div className="card-header"><h1>Sobre OPENCUP</h1></div>
+        <div className="card-body">
+          <blockquote className="blockquote mb-0">
+            <p>
+              OPENCUP nació como un torneo de tenis Amateur de Córdoba Capital, actualmente uno más grandes de la provincia. Cuenta con jugadores de la capital y alrededores. 
+            </p>
+            <p>Nuestra tienda nace como una necesidad de poder brindar más servicios a nuestros jugadores con la idea de comercializar sus productos ofreciendolos a otros competidores o al público en general</p>
+            <footer className="blockquote-footer">
+              Podes acceder a más información en nuestro sitio <cite title="Source Title">www.opencup.com.ar</cite>
+            </footer>
+          </blockquote>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SobreNosotros
+export default SobreNosotros;
